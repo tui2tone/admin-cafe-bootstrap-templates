97cfd423fe999411dd4059ef00db288c1122df1a
tui2tone <tui2tone@gmail.com>
2018-09-12 22:41:16 +0700

    [+] add card and button

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -42,13 +42,149 @@ $(document).ready(function() {
             datasets: [{
                 label: '',
                 radius: 0,
+                pointBorderWidth: 0,
+                pointBorderColor: '#FFF',
+                pointBackgroundColor: '#8BC34A',
+                pointHoverRadius: 10,
+                pointHoverBorderColor: '#FFF',
+                pointHoverBackgroundColor: '#FFF',
+                data: [4210 ,8010 ,5210 ,9750 ,12005 ,15035 ,16042 ,18251 ,28640 ,46000],
+                backgroundColor: [
+                    '#0d67b3CC'
+                ],
+                borderColor: [
+                    '#0d67b3CC'
+                ],
+                borderWidth: 0
+            }]
+        },
+        options: {
+            layout: {
+                padding: {
+                    left: -10,
+                    right: 0,
+                    top: -10,
+                    bottom: -10
+                }
+            },
+            legend: {
+                display: false
+            },
+            maintainAspectRatio: false,
+            scales: {
+                yAxes: [{
+                    ticks: {
+                        suggestedMin: 0,
+                        max: 50000,
+                        stepSize: 10000,
+                        beginAtZero:true,
+                        fontColor: '#AAA',
+                        display: false
+                    },
+                    gridLines: {
+                        drawBorder: false,
+                        color: "#F9F9F9"
+                    }
+                }],
+                xAxes: [{
+                    gridLines: {
+                        display: false,
+                        drawBorder: false,
+                        color: "#F9F9F9"
+                    },
+                    ticks: {
+                        fontColor: '#AAA',
+                        display: false,
+                        padding: 0
+                    }
+                }]
+            }
+        }
+    });
+
+    const ctx2 = document.getElementById("chart1").getContext('2d');
+    const myChart2 = new Chart(ctx2, {
+        type: 'line',
+        data: {
+            labels: ["10:40", "10:50", "11:00", "11:10", "11:20"],
+            datasets: [{
+                label: '',
+                radius: 0,
                 pointBackgroundColor: '#8BC34A',
                 pointHoverRadius: 10,
                 pointHoverBorderColor: '#8BC34A',
                 pointHoverBackgroundColor: '#FFF',
-                data: [4210 ,8010 ,5210 ,9750 ,12005 ,15035 ,16042 ,18251 ,28640 ,46000],
+                data: [4210 ,8010 ,5210 ,9750 ,12005],
                 backgroundColor: [
                     '#8BC34A33'
+                ],
+                borderColor: [
+                    '#8BC34A33'
+                ],
+                borderWidth: 3
+            }]
+        },
+        options: {
+            layout: {
+                padding: {
+                    left: -10,
+                    right: 0,
+                    top: 0,
+                    bottom: -10
+                }
+            },
+            bezierCurve: false,
+            legend: {
+                display: false
+            },
+            maintainAspectRatio: false,
+            scales: {
+                yAxes: [{
+                    ticks: {
+                        suggestedMin: 0,
+                        max: 15000,
+                        stepSize: 5000,
+                        beginAtZero:true,
+                        fontColor: '#AAA',
+                        display: false
+                    },
+                    gridLines: {
+                        display: false,
+                        drawBorder: false,
+                        color: "#EEE"
+                    }
+                }],
+                xAxes: [{
+                    gridLines: {
+                        display: false,
+                        drawBorder: false,
+                        color: "#EEE"
+                    },
+                    ticks: {
+                        fontColor: '#AAA',
+                        display: false,
+                        padding: 0
+                    }
+                }]
+            }
+        }
+    });
+
+    const ctx3 = document.getElementById("chart2").getContext('2d');
+    const myChart3 = new Chart(ctx3, {
+        type: 'line',
+        data: {
+            labels: ["10:40", "10:50", "11:00", "11:10", "11:20"],
+            datasets: [{
+                label: '',
+                radius: 0,
+                pointBackgroundColor: '#8BC34A',
+                pointHoverRadius: 10,
+                pointHoverBorderColor: '#8BC34A',
+                pointHoverBackgroundColor: '#FFF',
+                data: [4210 ,8010 ,5210 ,9750 ,12005],
+                backgroundColor: [
+                    '#8BC34A00'
                 ],
                 borderColor: [
                     '#8BC34A'
@@ -74,13 +210,14 @@ $(document).ready(function() {
                 yAxes: [{
                     ticks: {
                         suggestedMin: 0,
-                        max: 50000,
-                        stepSize: 10000,
+                        max: 15000,
+                        stepSize: 5000,
                         beginAtZero:true,
                         fontColor: '#AAA',
                         display: false
                     },
                     gridLines: {
+                        display: false,
                         drawBorder: false,
                         color: "#EEE"
                     }
